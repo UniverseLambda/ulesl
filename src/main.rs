8c27b74ca799d8527a69c26285a03805da7b5d70
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2023-05-20 12:56:03 +0200

    Launch args
    Input file is now taken from launch argument (no args or '-' set the input file as stdin)

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,29 @@ mod lexer;
 mod parser;
 mod vm;
 mod common;
+use std::io::Read;
 
 use lexer::Lexer;
 
 use crate::{parser::Parser, vm::Vm};
 
-
 fn main() {
 	// println!("[VM DEBUG] Hello, world!");
 
-	// let lex = Lexer::new(std::io::stdin(), "stdin".into());
-	let lex = Lexer::new(std::fs::File::open("./test.ulesl").unwrap(), "test.ulesl".into());
+	let mut args: Vec<String> = std::env::args().skip(1).collect();
+
+	if args.len() > 1 {
+		eprintln!("ulesl: Too many arguments");
+	}
+
+	let (reader, file): (Box<dyn Read>, String) =
+	if args.is_empty() || args[0] == "-" {
+		(Box::new(std::io::stdin()), "stdin".into())
+	} else {
+		(Box::new(std::fs::File::open(&args[0]).expect("ulesl: Could not open input file")), args.pop().unwrap())
+	};
+
+	let lex = Lexer::new(reader, file);
 	let mut parser = Parser::new(lex, "test.ulesl".into());
 	let mut vm = Vm::new();
 
